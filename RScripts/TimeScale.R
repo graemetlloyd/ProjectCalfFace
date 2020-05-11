ee1d1526f06165f0303ef319cf32ea2e158256b4
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-05-11 17:02:48 +0100

    Updated timescaling script.

--- RScripts/TimeScale.R
+++ RScripts/TimeScale.R
@@ -1,5 +1,9 @@
 # SCRIPT TO TIMESCALE TREE
 
+# TO DO:
+# - Spencer to add root age from https://onlinelibrary.wiley.com/doi/full/10.1002/spp2.1316 to Ken's node list.
+# - Graeme to add this to github https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0218791.
+
 # Load metatree library:
 library(paleotree)
 library(strap)
@@ -8,17 +12,55 @@ library(strap)
 Trees <- ape::read.tree("https://raw.githubusercontent.com/graemetlloyd/ProjectCalfFace/master/Trees/MPTs.tre")
 
 # Read in age data from GitHub:
-AgeData <- read.table("https://raw.githubusercontent.com/graemetlloyd/ProjectCalfFace/master/Recovered_Age_Data.csv", sep =",", header = TRUE)
+AgeData <- read.table("https://raw.githubusercontent.com/graemetlloyd/ProjectCalfFace/master/Recovered_Age_Data.csv", sep = ",", header = TRUE)
 
 # First reformat step for paleotree:
 PaleotreeAgeData <- AgeData[!is.na(AgeData[, "FAD"]), c("X...Taxon", "FAD", "LAD")]
 
 # Second reformat step for paleotree:
-PaleotreeAgeData  <- matrix(c(PaleotreeAgeData[, "FAD"], PaleotreeAgeData[, "LAD"]), ncol = 2, dimnames = list(PaleotreeAgeData[, "X...Taxon"], c("FAD", "LAD")))
+PaleotreeAgeData <- matrix(c(PaleotreeAgeData[, "FAD"], PaleotreeAgeData[, "LAD"]), ncol = 2, dimnames = list(PaleotreeAgeData[, "X...Taxon"], c("FAD", "LAD")))
 
-tree = Trees[[1]]
+# Read in node age pairs:
+NodeAgePairs <- read.table("https://raw.githubusercontent.com/graemetlloyd/ProjectCalfFace/master/node_dates_from_ken.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
-NodeListWithDates <- read.table("https://raw.githubusercontent.com/graemetlloyd/ProjectCalfFace/master/min_node_dates_for_r.csv", header = TRUE)
+# Silly fudgy deleting bits required for now to make things work:
+NodeAgePairs <- NodeAgePairs[-28, ]
+PaleotreeAgeData <- PaleotreeAgeData[-match(setdiff(rownames(PaleotreeAgeData), sort(Trees[[1]]$tip.label)), rownames(PaleotreeAgeData)), ]
+
+# For each tree in turn:
+for(i in 1:length(Trees)) {
+  
+  # Isolate ith tree:
+  tree <- Trees[[i]]
+  
+  # Prunes taxa with no ages:
+  tree <- paleotree::timePaleoPhy(tree, timeData = PaleotreeAgeData, type = "equal", vartime = 1)
+  
+  # Get number of tips in tree (after pruning)
+  NTips <- ape::Ntip(tree)
+  
+  # Crwate node minimum age vector:
+  nodeminima <- rep(NA, tree$Nnode)
+  
+  # For each minimum node age pair:
+  for(j in 1:nrow(NodeAgePairs)) {
+    
+    # Fidn node number and store minimum age in vector:
+    nodeminima[(Claddis::FindAncestor(descs = NodeAgePairs[j, c("Taxon_1", "Taxon_2")], tree = tree) - NTips)] <- NodeAgePairs[j, "Min_Date"]
+    
+  }
+  
+  # Time-scale tree and store in Trees list:
+  Trees[[i]] <- paleotree::timePaleoPhy(tree, timeData = PaleotreeAgeData, type = "equal", vartime = 1, node.mins = nodeminima)
+  
+}
+
+
+
+# GRAEME HAS NOT LOOKED BELOW HERE
+
+
+
 
 
 

e0e859bd234adf17d5649c6b387f09ec02637062
Graeme T. Lloyd <graemetlloyd@gmail.com>
2020-08-06 17:30:55 +0100

    Updating R scripts with new Claddis funtion names.

--- RScripts/BuildFullMPTs.R
+++ RScripts/BuildFullMPTs.R
@@ -10,7 +10,7 @@ AllSTRMPTs <- AllSTRMPTs[grep("\\(allzero", AllSTRMPTs)]
 write(gsub(" ", "", AllSTRMPTs), "~/Desktop/Desktop - Spencer???s MacBook Pro (2)/NSF metatree/ProjectCalfFace/TNTTrees/AllSTRMPTs.tre")
 
 # Safely reinsert taxa and write out to file:
-Claddis::SafeTaxonomicReinsertion(treefile.in = "~/Desktop/Desktop - Spencer???s MacBook Pro (2)/NSF metatree/ProjectCalfFace/TNTTrees/AllSTRMPTs.tre", treefile.out = "~/Desktop/Desktop - Spencer???s MacBook Pro (2)/NSF metatree/ProjectCalfFace/Trees/MPTs.tre", str.list = read.table("~/Desktop/Desktop - Spencer???s MacBook Pro (2)/NSF metatree/ProjectCalfFace/Metatree/STR.txt", header = TRUE, stringsAsFactors = FALSE), multi.placements = "random")
+Claddis::safe_taxonomic_reinsertion(input_filename = "~/Desktop/Desktop - Spencer???s MacBook Pro (2)/NSF metatree/ProjectCalfFace/TNTTrees/AllSTRMPTs.tre", output_filename = "~/Desktop/Desktop - Spencer???s MacBook Pro (2)/NSF metatree/ProjectCalfFace/Trees/MPTs.tre", str_taxa = read.table("~/Desktop/Desktop - Spencer???s MacBook Pro (2)/NSF metatree/ProjectCalfFace/Metatree/STR.txt", header = TRUE, stringsAsFactors = FALSE), multiple_placement_option = "random")
 
 # Get strict consensuss and write to file:
 AllMPTs <- ape::read.tree("~/Desktop/Desktop - Spencer???s MacBook Pro (2)/NSF metatree/ProjectCalfFace/Trees/MPTs.tre")
